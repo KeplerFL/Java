716cfe1a0450b2e6c6786c6a73320d0dbc2a4581
Kepler <mattiag@anche.no>
2013-02-28 23:19:53 +0100

    [New Project] Ruzzle dictionary Creator

---
+++ elimina/src/elimina/indice.java
@@ -0,0 +1,14 @@
+package elimina;
+
+import java.io.Serializable;
+
+public class indice implements Serializable{
+	char l;
+	int n;
+	
+	public indice(char c, int n)
+	{
+		l = c;
+		this.n = n;
+	}
+}
