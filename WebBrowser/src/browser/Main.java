68e8ba7473d4a5d217d5e7511175debb3a193ee0
kepler <matti.graziani94@gmail.com>
2014-01-14 09:33:21 +0100

    Branch first creation and TextEditor project, in addition to improvement to Ruzzle

---
+++ WebBrowser/src/browser/Main.java
@@ -0,0 +1,14 @@
+package browser;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setTitle("Java Browser");
+		f.setSize(500, 500);
+		f.setLocation(100, 100);
+	}
+
+}
